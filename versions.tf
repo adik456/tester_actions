56a44bfb14c54a21199de859fcfa4af8f32f8300
Adrian Ginalski <adrian.ginalski1@pl.abb.com>
2025-11-28 12:45:34 +0100

    Update S3 bucket configuration and tags in Terraform files

--- versions.tf
+++ versions.tf
@@ -1,4 +1,11 @@
 terraform {
+   backend "s3" {
+    bucket = "ag-my-terraform-state-bucket"
+    key    = "./terraform.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "tk0-terraform-locks"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
