b47df70bf07ca2f706db7ea6958955985d6de47c
Adrian Ginalski <adrian.ginalski1@pl.abb.com>
2025-11-28 12:53:51 +0100

    Fix S3 bucket name in Terraform configuration

--- versions.tf
+++ versions.tf
@@ -1,11 +1,13 @@
 terraform {
-   backend "s3" {
-    bucket = "ag-my-terraform-state-bucket"
+  backend "s3" {
+    bucket = "tk0-my-terraform-state-bucket"
     key    = "./terraform.tfstate"
     region = "eu-central-1"
     dynamodb_table = "tk0-terraform-state-lock-dynamo"
     encrypt = true
   }
+
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
